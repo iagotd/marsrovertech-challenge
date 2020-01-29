eee706e09d6bc452f0f37397cd0065ef69b5ca55
iagotd <iagotudela@gmail.com>
2020-01-29 20:02:43 +0100

    First commit

---
+++ src/main/java/domain/Rover.java
@@ -0,0 +1,10 @@
+package domain;
+
+public class Rover {
+    public Rover(String input) {
+    }
+
+    public String execute() {
+        return "";
+    }
+}
---
+++ src/test/java/GoldenTest.java
@@ -0,0 +1,27 @@
+import domain.Rover;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class GoldenTest {
+    @Test
+    void shouldReturnExpected (){
+        String input = "5 5\n" +
+                "\n" +
+                "1 2 N\n" +
+                "\n" +
+                "LMLMLMLMM\n" +
+                "\n" +
+                "3 3 E\n" +
+                "\n" +
+                "MMRMMRMRRM";
+
+        String expected = "1 3 N\n" +
+                "\n" +
+                "5 1 E";
+
+        String actual = new Rover(input).execute();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
